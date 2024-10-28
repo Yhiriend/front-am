c9c782be8d2039a61b21cac357b647e15630af4e
yhiriend <yirien45@gmail.com>
2024-10-27 23:12:34 -0500

    update env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 const urlBaseDev = 'http://localhost:3002';
-const urlBaseProd = 'https://back-am-production.up.railway.app';
+const urlBaseProd = 'https://memoriplay-back-production.up.railway.app';
 
 export const environment = {
   users: `${urlBaseProd}/api/users`,
   acitivity: `${urlBaseProd}/api/activity`,
-  secretKey: 'K%@Tt48aeAdydX(9p!4q',
+  secretKey: 'rN4Z3yn96rJLv!zzB(+q',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  urlBase: 'https://back-am-production.up.railway.app',
-  users: 'https://back-am-production.up.railway.app/api/users',
-  acitivity: 'https://back-am-production.up.railway.app/api/activity',
-  secretKey: 'K%@Tt48aeAdydX(9p!4q',
+  urlBase: 'https://memoriplay-back-production.up.railway.app',
+  users: 'https://memoriplay-back-production.up.railway.app/api/users',
+  acitivity: 'https://memoriplay-back-production.up.railway.app/api/activity',
+  secretKey: 'rN4Z3yn96rJLv!zzB(+q',
 };
